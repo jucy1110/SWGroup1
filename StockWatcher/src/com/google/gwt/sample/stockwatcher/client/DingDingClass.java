eb5b3e27042e7828a5e03157f9c91d1bc1d8faa8
jucy1110 <jucy1110@gmail.com>
2015-01-29 15:00:51 -0800

    karin commit2

--- StockWatcher/src/com/google/gwt/sample/stockwatcher/client/DingDingClass.java
+++ StockWatcher/src/com/google/gwt/sample/stockwatcher/client/DingDingClass.java
@@ -2,8 +2,13 @@ package com.google.gwt.sample.stockwatcher.client;
 
 public class DingDingClass {
      Integer totalStock;
+     Integer totalChange;
      
      public void setTotalStock(Integer n){
     	 this.totalStock = n+2;
+    	 if (n > 3){
+    		 totalChange++;
+    	 }
+    	 
      }
 }
