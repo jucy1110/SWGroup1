97fea130879b9422aac45fbdefb01772f6c1008d
jucy1110 <jucy1110@gmail.com>
2015-01-29 16:21:23 -0800

    commit

--- StockWatcher/src/com/google/gwt/sample/stockwatcher/client/DingDingClass.java
+++ StockWatcher/src/com/google/gwt/sample/stockwatcher/client/DingDingClass.java
@@ -1,9 +1,11 @@
 package com.google.gwt.sample.stockwatcher.client;
 
-public class DingDingClass {
+public class DingDingClass implements DingDing{
      Integer totalStock;
      
+     @Override
      public void setTotalStock(Integer n){
     	 this.totalStock = n+2;
      }
+
 }
